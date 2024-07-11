195d8e55aa6d1e34bfea348a3da9507af9b8b0c0
kimji <jhkim@youngjintech.com>
2024-07-11 18:23:38 +0900

    블로그 생성

---
+++ blog/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+<!--  <button type="button" class="btn btn-primary">Primary</button>-->
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link" href="#">Features</a>
+          <a class="nav-link" href="#">Pricing</a>
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        </div>
+      </div>
+    </div>
+  </nav>
+
+  <div>
+    <h5>React 개발자의 블로그 입니다.</h5>
+    <p>- Vue -</p>
+  </div>
+
+  <List
+    :blogs="blogs"
+  />
+
+</template>
+
+<script>
+import List from '@/components/List.vue';
+import blog from './mock/Data';
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      blogs : blog,
+    }
+  },
+  components: {
+    List : List,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ blog/src/components/List.vue
@@ -0,0 +1,25 @@
+<template>
+  <hr/>
+
+  <div v-for="blogs in blogs" :key="blogs">
+    <h5>{{blogs.title}}</h5>
+    <p>{{ blogs.date }}</p>
+    <p>{{ blogs.content }}</p>
+    <p>{{ blogs.number }}</p>
+    <hr/>
+  </div>
+</template>
+<script>
+
+
+export default {
+  name: 'List',
+  props: {
+    blogs: Array,
+  }
+}
+
+</script>
+<style>
+
+</style>
