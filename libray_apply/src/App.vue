e15225fb05aa1f1ccef86519b214ef52fe95be7b
kimji <jhkim@youngjintech.com>
2024-07-18 16:11:33 +0900

    json server 와 dev 같이 시키기

--- libray_apply/src/App.vue
+++ libray_apply/src/App.vue
@@ -1,11 +1,53 @@
 <template>
-  <v-app>
-    <v-main>
-      <router-view />
-    </v-main>
-  </v-app>
+  <router-view/>
 </template>
 
 <script setup>
-  //
 </script>
+
+<style>
+html{
+  font-family: 'SUITE-Reguar',sans-serif;
+}
+
+@font-face {
+  font-family: 'SUITE-Reguar';
+  src: url("src/assets/font/SUIT-woff2/SUIT-Regular.woff2")format('woff2');
+}
+
+@font-face {
+  font-family: 'SUITE-Bold';
+  src:url("src/assets/font/SUIT-woff2/SUIT-Bold.woff2")format('woff2') ;
+  font-weight: 400;
+  font-style: normal;
+}
+
+@font-face {
+  font-family: 'LINE-KR-Rg';
+  src: url("src/assets/font/LINE-KR/LINESeedKR-Rg.woff2") format('woff2');
+  font-weight: 400;
+  font-style: normal;
+}
+
+@font-face {
+  font-family: 'LINE-KR-Bd';
+  src: url("src/assets/font/LINE-KR/LINESeedKR-Bd.woff2") format('woff2');
+  font-weight: 400;
+  font-style: normal;
+}
+
+@font-face {
+  font-family: 'LINE-EN-Rg';
+  src: url("src/assets/font/LINE-EN/LINESeedSans_W_Rg.woff2") format('woff2');
+  font-weight: 400;
+  font-style: normal;
+}
+
+@font-face {
+  font-family: 'LINE-EN-Bd';
+  src: url("src/assets/font/LINE-EN/LINESeedSans_W_Bd.woff2") format('woff2');
+  font-weight: 400;
+  font-style: normal;
+}
+
+</style>
--- libray_apply/src/pages/index.vue
+++ libray_apply/src/pages/index.vue
@@ -1,7 +1,11 @@
 <template>
-
+안녕하세요 김지희 입니다.
 </template>
 
 <script setup>
 
 </script>
+<style>
+
+
+</style>
