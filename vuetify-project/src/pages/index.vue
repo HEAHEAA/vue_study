0fbc4523a338f8f183bf3fc6b821dcaa0e478786
jiheekim <jhkim@youngjintech.com>
2024-07-24 21:42:09 +0900

    pinia 사용해보기

---
+++ vuetify-project/src/pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <p>{{name}}</p>
+  <p>Count {{count}}</p>
+  <button @click="increment">숫자 +1</button>
+  <br/>
+  <button @click="nameChange">이름바꾸기</button>
+</template>
+
+<script setup>
+ import {useCounterStore} from "@/store/counter";
+ import {storeToRefs} from "pinia";
+ const store = useCounterStore();
+
+ const {name, count} = storeToRefs(store);
+ const {increment,nameChange} = store
+
+</script>
+<style>
+button{
+  background-color: aqua;
+}
+</style>
