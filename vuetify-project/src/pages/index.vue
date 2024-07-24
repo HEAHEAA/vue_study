1e24d8d987393a51c29c894fd275704937efc8e1
jiheekim <jhkim@youngjintech.com>
2024-07-24 21:45:10 +0900

    value 변경

--- vuetify-project/src/pages/index.vue
+++ vuetify-project/src/pages/index.vue
@@ -1,18 +1,26 @@
 <template>
-  <p>{{name}}</p>
   <p>Count {{count}}</p>
   <button @click="increment">숫자 +1</button>
   <br/>
-  <button @click="nameChange">이름바꾸기</button>
+  <p>{{name}}</p>
+  <button @click="changeName">이름바꾸기</button>
 </template>
 
 <script setup>
  import {useCounterStore} from "@/store/counter";
  import {storeToRefs} from "pinia";
+ import {useUserStore} from "@/store/user";
  const store = useCounterStore();
+ const user = useUserStore();
 
- const {name, count} = storeToRefs(store);
- const {increment,nameChange} = store
+ const {count} = storeToRefs(store);
+ const {name} = storeToRefs(user);
+ const {increment} = store
+ const {setName} = user;
+
+ const changeName = () => {
+   setName('지희');
+ }
 
 </script>
 <style>
